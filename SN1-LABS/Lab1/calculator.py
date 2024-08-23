eee140e8166ae653c90703aecc342f6a4666103c
Max Allan Jonathan Fotchin <onlinepromax12@gmail.com>
2024-08-23 14:49:30 -0400

    created calculator.py and add function getNumber

---
+++ SN1-LABS/Lab1/calculator.py
@@ -0,0 +1,16 @@
+def getNumber(s: str, index: int = 0):
+    i = index
+    commaFound = False
+    while (i < len(s)):
+        if (x[i] == "."):
+            if commaFound:
+                raise Exception("There should not be two comma in a number")
+            commaFound = True
+        elif(not (x[i].isnumeric() or (x[i] == "-" and i == 0))):
+            break
+        i+=1
+    return [i, float(s[index:i])]
+
+
+x = input()
+print(f"The number is {getNumber(x)}")
