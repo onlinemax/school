834a4290a2d6d227e5f0847b1434443599020f11
onlinemax <onlinepromax12@gmail.com>
2024-11-08 20:05:00 -0500

    Finished lab14

--- SN1-LABS/lab14/Lab14-p3.py
+++ SN1-LABS/lab14/Lab14-p3.py
@@ -4,4 +4,4 @@ status=['full-time','temporary','part-time','executive','study-leave',
         'maternity-leave','full-time','part-time']
 vac_pay=[4.0,4.25,5.5,4.75,3.6,4.9,4.5,5.25]
 for i in range(len(employee)):
-    print(f"{employee[i]:<10}{salary[i]:^11}{vac_pay[i]:<6.2f}{status[i]}")
+    print(f"{employee[i]:10}{salary[i]:9}{vac_pay[i]:6.2f}  {status[i]}")
